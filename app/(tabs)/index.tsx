9a20fd38d6f2d2f542954d2ff5439edab33b64c8
ahmetpalavan <ahmetpalavann@gmail.com>
2023-11-11 17:03:59 +0300

    feat: Update code formatting and imports
    - Reformat code to follow consistent import statement style
    - Update import statements to use double quotes instead of single quotes for consistency
    - Remove unused imports and comments

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,78 @@
-import { StyleSheet } from 'react-native';
+import { ActivityIndicator, Button, FlatList, ListRenderItem, StyleSheet, TextInput, Touchable, TouchableOpacity } from "react-native";
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import { Text, View } from "../../components/Themed";
+import { Todo, getTodos } from "../../api/todos";
+import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function TabOneScreen() {
+  const [todos, setTodos] = useState("");
+  const todosQuery = useQuery({
+    queryKey: ["todos"],
+    queryFn: getTodos,
+  });
+
+  useEffect(() => {
+    todosQuery.refetch();
+    console.log("🚀 ~ file: index.tsx:16 ~ useEffect ~ todosQuery:", todosQuery.data);
+  }, []);
+
+  const renderTodo: ListRenderItem<Todo> = ({ item }) => {
+    const deleteTodo = () => {};
+    const updateTodo = () => {};
+    return (
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          margin: 5,
+          padding: 10,
+          gap: 10,
+          marginVertical: 10,
+        }}
+      >
+        <TouchableOpacity onPress={() => {}}>
+          {item.done ? (
+            <Ionicons name="checkmark-circle" size={24} color="green" />
+          ) : (
+            <Ionicons name="checkmark-circle-outline" size={24} color="black" />
+          )}
+          <Text>{item.text}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={deleteTodo} style={{ marginLeft: 10 }}>
+          <Ionicons name="trash" size={24} color="red" />
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
+        <TextInput placeholder="New Todo" onChangeText={setTodos} value={todos} />
+        <Button title="Add Todo" onPress={() => {}} />
+      </View>
+      {todosQuery.isLoading ? <ActivityIndicator size={"large"} /> : null}
+      {todosQuery.isError ? <Text>Error</Text> : null}
+      <FlatList data={todosQuery.data} renderItem={renderTodo} keyExtractor={(item) => item.id.toString()} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 10,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
